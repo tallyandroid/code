2dfb8d599aa8f5aa7eb6a2de66899b24f1190113
Christian Tisby <cduane82@hotmail.com>
2020-03-30 00:37:49 -0400

    adding c# arguments

---
+++ chapter02/arguments/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using static System.Console;
+namespace arguments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine($"There are {args.Length} arguments.");
+            if (args.Length < 4)
+{
+  WriteLine("You must specify two colors and dimensions, e.g.");
+  WriteLine("dotnet run red yellow 80 40");
+  return; // stop running
+}
+ForegroundColor = (ConsoleColor)Enum.Parse(
+  enumType: typeof(ConsoleColor), 
+  value: args[0], 
+  ignoreCase: true);
+BackgroundColor = (ConsoleColor)Enum.Parse(
+  enumType: typeof(ConsoleColor), 
+  value: args[1], 
+  ignoreCase: true);
+WindowWidth = int.Parse(args[2]);
+WindowHeight = int.Parse(args[3]);
+        }
+    }
+}
--- chapter02/nullhandling/Program.cs
+++ chapter02/nullhandling/Program.cs
@@ -1,12 +1,33 @@
-﻿using System;
+﻿#nullable enable
+using System;
 
 namespace nullhandling
 {
+    class Address
+{
+  public string? Building;
+  public string Street = string.Empty;
+  public string City = string.Empty; 
+  public string Region = string.Empty;
+}
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+          int thisCannotBeNull = 4;
+        //   thisCannotBeNull = null; // compile error!
+          int? thisCouldBeNull = null;
+          Console.WriteLine(thisCouldBeNull);
+          Console.WriteLine(thisCouldBeNull.GetValueOrDefault());
+          thisCouldBeNull = 7;
+          Console.WriteLine(thisCouldBeNull);
+          Console.WriteLine(thisCouldBeNull.GetValueOrDefault());
+
+          var address = new Address();
+          address.Building = null;
+          address.Street = null;
+          address.City = "London";
+          address.Region = null;
         }
     }
 }
